50f38b9ec1407cc399ffdceb08334d8ebd5e0ff5
Jeff Hall <jeff.leon.hall@gmail.com>
2021-03-03 01:27:02 -0700

    display the owner's name, number of cars, and average price for those cars, where the number of cars must be at least 2 and the average price must be more than 10000

--- queries.sql
+++ queries.sql
@@ -3,7 +3,7 @@
 
 SELECT * FROM owners o 
 LEFT JOIN vehicles v 
-ON v.owner_id = o.id
+ON v.owner_id = o.id;
 
 -- In this query, we count the number of cars for each owner and display the results in ascending order by the owner's first name.
 
@@ -13,3 +13,13 @@ JOIN vehicles v
 ON v.owner_id = o.id 
 GROUP BY o.first_name, o.last_name
 ORDER BY o.first_name;
+
+-- With this query, we select the owner's name along with the number of cars that they own, and the average price for those cars.
+
+SELECT o.first_name, o.last_name, ROUND(AVG(price)) as average_price, COUNT(*) 
+FROM owners o 
+JOIN vehicles v 
+ON v.owner_id = o.id 
+GROUP BY o.first_name, o.last_name 
+HAVING COUNT(*) > 1 AND ROUND(AVG(price)) > 10000
+ORDER BY o.first_name DESC;
