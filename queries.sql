6acf528fafef159e958cab190527037f8143234c
Jeff Hall <jeff.leon.hall@gmail.com>
2021-03-03 00:27:33 -0700

    count the number of cars for each owner

--- queries.sql
+++ queries.sql
@@ -4,3 +4,12 @@
 SELECT * FROM owners o 
 LEFT JOIN vehicles v 
 ON v.owner_id = o.id
+
+-- In this query, we count the number of cars for each owner and display the results in ascending order by the owner's first name.
+
+SELECT o.first_name, o.last_name, COUNT(*) AS count 
+FROM owners o 
+JOIN vehicles v 
+ON v.owner_id = o.id 
+GROUP BY o.first_name, o.last_name
+ORDER BY o.first_name;
