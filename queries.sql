0d58824b130f50514d1f8450f9c761a0b71326d8
Jeff Hall <jeff.leon.hall@gmail.com>
2021-03-03 00:11:47 -0700

    combined the tables with a left join so that the owner without a car appears in the table

--- queries.sql
+++ queries.sql
@@ -1,1 +1,6 @@
--- write your queries here
+-- This query will join the tables together but will perform a left join. That is, it will match all cars with their owner's information
+-- and will include any owners that do not have a matching owner id.
+
+SELECT * FROM owners o 
+LEFT JOIN vehicles v 
+ON v.owner_id = o.id
